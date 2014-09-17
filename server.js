3ac52654d66f4c780b709358686888864a20c39a
benscott <ben@benscott.co.uk>
2014-09-17 20:23:23 +0100

    Added passport

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 	// set up ========================
 	var express  = require('express');
     var fs       = require('fs');
-	var app      = express(); 								// create our app w/ express
-	var mongoose = require('mongoose'); 					// mongoose for mongodb
-	var morgan = require('morgan'); 			// log requests to the console (express4)
-	var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
-	var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+	var app      = express();
+	var mongoose = require('mongoose');
+    var passport = require('passport');
+
+    var morgan = require('morgan');
+	var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
+    var cookieParser = require('cookie-parser');
+    var session      = require('express-session')
 
 	// configuration =================
 
@@ -22,13 +26,21 @@
     app.use(bodyParser.json()); // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-
+    app.use(cookieParser());
     app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
     app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+    // required for passport
+    app.use(session({ secret: 'science-uncovered' })); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+
+    require('./config/passport')(passport);
 
     // routes ==================================================
     require('./app/routes/specimens')(app);
     require('./app/routes/twitter')(app);
+    require('./app/routes/login')(app, passport);
     require('./app/routes/index')(app);
 
     // start app ===============================================
