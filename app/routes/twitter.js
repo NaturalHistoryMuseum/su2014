6e5039c3dce53deedb1818e61507d90194fc201b
benscott <ben@benscott.co.uk>
2014-09-17 20:22:35 +0100

    temp

--- app/routes/twitter.js
+++ app/routes/twitter.js
@@ -3,38 +3,40 @@ module.exports = function (app) {
     // Send a tweet
     app.post('/tweet', function (req, res) {
 
+        console.log("TWEET");
 
-        var data = req.body
-        data.username = "@benjaminscott"
 
-        var keys = require('../../config/twitter-auth');
+//        var data = req.body
+//        data.username = "@benjaminscott"
+//
+//        var keys = require('../../config/twitter-auth');
+//
+//        var OAuth = require('oauth');
+//
+//        var twitterOAuth = new OAuth.OAuth(
+//            "https://api.twitter.com/oauth/request_token",
+//            "https://api.twitter.com/oauth/access_token",
+//            keys.consumerKey,
+//            keys.consumerSecret,
+//            "1.0A",
+//            null,
+//            "HMAC-SHA1"
+//        );
+//
+//        var status = '.' + data.username + ' has just transcribed @nhm_london specimen ' + data.flickrURL + ' #su2014'
+//
+//        var body = ({'status': status});
 
-        var OAuth = require('oauth');
-
-        var twitterOAuth = new OAuth.OAuth(
-            "https://api.twitter.com/oauth/request_token",
-            "https://api.twitter.com/oauth/access_token",
-            keys.consumerKey,
-            keys.consumerSecret,
-            "1.0A",
-            null,
-            "HMAC-SHA1"
-        );
-
-        var status = '.' + data.username + ' has just transcribed @nhm_london specimen ' + data.flickrURL + ' #su2014'
-
-        var body = ({'status': status});
-
-        twitterOAuth.post("https://api.twitter.com/1.1/statuses/update.json",
-            keys.accessToken, keys.accessTokenSecret, body, "application/json",
-            function (error, data, response2) {
-                if (error) {
-                    console.log('Error: Something is wrong.\n' + JSON.stringify(error) + '\n');
-                } else {
-                    console.log('Twitter status updated.\n');
-                    console.log(response2 + '\n');
-                }
-            });
+//        twitterOAuth.post("https://api.twitter.com/1.1/statuses/update.json",
+//            keys.accessToken, keys.accessTokenSecret, body, "application/json",
+//            function (error, data, response2) {
+//                if (error) {
+//                    console.log('Error: Something is wrong.\n' + JSON.stringify(error) + '\n');
+//                } else {
+//                    console.log('Twitter status updated.\n');
+//                    console.log(response2 + '\n');
+//                }
+//            });
 
     });
 
