0b3024899bc01f56771f7b03d0017e6a3125e309
benscott <ben@benscott.co.uk>
2014-09-18 16:39:11 +0100

    Renamed login:auth

--- app/routes/twitter.js
+++ app/routes/twitter.js
@@ -1,42 +1,47 @@
+var bcrypt   = require('bcrypt-nodejs');
+
 module.exports = function (app) {
 
     // Send a tweet
     app.post('/tweet', function (req, res) {
 
-        console.log("TWEET");
+        if (bcrypt.compareSync('informaticians', req.cookies['su2014-key'])) {
 
+            var data = req.body
+            data.username = "@benjaminscott"
 
-//        var data = req.body
-//        data.username = "@benjaminscott"
-//
-//        var keys = require('../../config/twitter-auth');
-//
-//        var OAuth = require('oauth');
-//
-//        var twitterOAuth = new OAuth.OAuth(
-//            "https://api.twitter.com/oauth/request_token",
-//            "https://api.twitter.com/oauth/access_token",
-//            keys.consumerKey,
-//            keys.consumerSecret,
-//            "1.0A",
-//            null,
-//            "HMAC-SHA1"
-//        );
-//
-//        var status = '.' + data.username + ' has just transcribed @nhm_london specimen ' + data.flickrURL + ' #su2014'
-//
-//        var body = ({'status': status});
+            var keys = require('../../config/twitter-auth');
 
-//        twitterOAuth.post("https://api.twitter.com/1.1/statuses/update.json",
-//            keys.accessToken, keys.accessTokenSecret, body, "application/json",
-//            function (error, data, response2) {
-//                if (error) {
-//                    console.log('Error: Something is wrong.\n' + JSON.stringify(error) + '\n');
-//                } else {
-//                    console.log('Twitter status updated.\n');
-//                    console.log(response2 + '\n');
-//                }
-//            });
+            var OAuth = require('oauth');
+
+            var twitterOAuth = new OAuth.OAuth(
+                "https://api.twitter.com/oauth/request_token",
+                "https://api.twitter.com/oauth/access_token",
+                keys.consumerKey,
+                keys.consumerSecret,
+                "1.0A",
+                null,
+                "HMAC-SHA1"
+            );
+
+            var status = '.' + data.username + ' has just transcribed @nhm_london specimen ' + data.flickrURL + ' #su2014'
+
+            var body = ({'status': status});
+
+            twitterOAuth.post("https://api.twitter.com/1.1/statuses/update.json",
+                keys.accessToken, keys.accessTokenSecret, body, "application/json",
+                function (error, data, response2) {
+                    if (error) {
+                        console.log('Error: Something is wrong.\n' + JSON.stringify(error) + '\n');
+                    } else {
+                        console.log('Twitter status updated.\n');
+                        console.log(response2 + '\n');
+                    }
+                });
+
+        }else{
+            console.log('ERROR: Cookie does not match')
+        }
 
     });
 
