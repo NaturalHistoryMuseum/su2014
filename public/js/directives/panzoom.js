99c1006b5059f0eb75e4b0aec297552564e32657
benscott <ben@benscott.co.uk>
2014-09-22 18:29:12 +0100

    Added panzoom

---
+++ public/js/directives/panzoom.js
@@ -0,0 +1,36 @@
+angular.module('panzoom', []).directive('panzoom', function() {
+    return {
+        // Restrict it to be an attribute in this case
+        restrict: 'A',
+        // responsible for registering DOM listeners as well as updating the DOM
+        link: function(scope, element) {
+
+            $(element).panzoom({
+                minScale: 1
+            });
+
+//            TODO: Zoom out
+
+            console.log($(element).find('img').clientHeight)
+
+            $(element).find('img').load(function() {
+
+                var $img = $(element).find('img')
+
+                $img.css('height', $(this).height() + 'px');
+                $img.css('max-height', 'none');
+
+
+            });
+
+            $(element).find('img').addSwipeEvents().bind('doubletap', function(evt, touch) {
+                $(element).panzoom("zoom", {
+                     focal: touch.touch,
+                     animate: true,
+                     increment: 0.5
+                });
+            });
+
+        }
+    };
+});
