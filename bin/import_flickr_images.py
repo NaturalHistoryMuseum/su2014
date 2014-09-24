aa75f42c9e12852823cba35385eb1542c47333fb
benscott <ben@benscott.co.uk>
2014-09-24 18:37:01 +0100

    Fixed to use group + last date

--- bin/import_flickr_images.py
+++ bin/import_flickr_images.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# Import images from FLICKR
-# This should be run on cron, every 5 mins
+"""
+Import images from FLICKR
 
+Either load from group:
+
+python bin/import_flickr_images.py -g
+
+Or (on the night) load for a user (specify uploaded since timestamp)
+This should be run on cron, every 5 mins
+
+python bin/import_flickr_images.py -u 1411479000
+
+
+
+"""
+
+
+import sys
+import getopt
 import json
 from pymongo import MongoClient
 from pymongo.errors import InvalidOperation
@@ -12,8 +28,8 @@ from flickr_api.api import flickr
 FLICKR_API_KEY = '3608750bb8733644f1b6b0ea75602330'
 FLICKR_API_SECRET = '3399d3a72bf1023c'
 FLICKR_USER_ID = '53906044@N04'
+FLICKR_GROUP_ID = '2687808@N24'
 FLICKR_PER_PAGE = 10
-FLICKR_TAG = 'SU2014'
 
 MONGO_DB = 'su2014'
 MONGO_COLLECTION = 'specimens'
@@ -21,38 +37,90 @@ MONGO_COLLECTION = 'specimens'
 client = MongoClient()
 db_collection = client[MONGO_DB][MONGO_COLLECTION]
 
-def get_existing_images():
+help_message = '''
+    Import flickr images into mongo
+'''
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hgu:v", ["help", "group", "upload-min-date="])
+        except getopt.error, msg:
+            raise Usage(msg)
+        args = {}
+        # option processing
+        for option, value in opts:
+            if option == "-v":
+                args['verbose'] = True
+            if option in ("-h", "--help"):
+                raise Usage(help_message)
+            if option in ("-g", "--group"):
+                args['group'] = True
+            if option in ("-u", "--upload-min-date"):
+                args['upload_min_date'] = value
+
+        import_flickr_images(**args)
+
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
+
+
+def get_existing_images(group=False):
     """
     Get all existing images from MongoDB
     """
-    cursor = db_collection.find({}, {'_id': 1})
+
+    if group:
+        q = {'group': FLICKR_GROUP_ID}
+    else:
+        q = {'group': {'$exists': False}}
+
+    cursor = db_collection.find(q, {'_id': 1})
     return [record['_id'] for record in cursor]
 
 
-def import_flickr_images():
-
+def import_flickr_images(verbose=False, group=False, upload_min_date=None):
     """
     Gets public photos from the Flickr group
     And adds them to mongo
     """
 
     flickr_api.set_keys(api_key=FLICKR_API_KEY, api_secret=FLICKR_API_SECRET)
-
-    existing_images = get_existing_images()
+    existing_images = get_existing_images(group)
 
     current_page = (len(existing_images) / FLICKR_PER_PAGE) + 1
 
     print 'Loading flickr images for page %s' % current_page
 
-    response = json.loads(flickr.photos.search(
-        user_id=FLICKR_USER_ID,
-        tags=FLICKR_TAG,
-        format='json',
-        nojsoncallback=1,
-        per_page=FLICKR_PER_PAGE,
-        page=current_page,
-        extras='url_l, url_o, path_alias'
-    ))
+    params = {
+        'format': 'json',
+        'nojsoncallback': 1,
+        'per_page': FLICKR_PER_PAGE,
+        'page': current_page,
+        'extras': 'url_l, url_o, path_alias'
+    }
+
+    if group:
+        func = flickr.groups.pools.getPhotos
+        params['group_id'] = FLICKR_GROUP_ID
+    elif upload_min_date:
+        func = flickr.photos.search
+        params['user_id'] = FLICKR_USER_ID
+        params['min_upload_date'] = upload_min_date
+    else:
+        raise Usage('If importing from user photostream, please specify min upload date')
+
+    response = json.loads(func(**params))
 
     bulk = db_collection.initialize_unordered_bulk_op()
 
@@ -66,9 +134,12 @@ def import_flickr_images():
 
         if photo['id'] not in existing_images:
 
+            if verbose:
+                print photo
+
             record = dict(
                 _id=photo['id'],
-                flickrURL='http://www.flickr.com/photos/' + photo['pathalias'] + '/' + photo['id']
+                flickrURL='http://www.flickr.com/photos/' + photo['pathalias'] + '/' + photo['id'],
             )
 
             # Not all photos have url_l - if not use the original
@@ -77,10 +148,13 @@ def import_flickr_images():
             except KeyError:
                 record['url'] = photo['url_o']
 
+            # If group specified, add the group
+            if group:
+                record['group'] = FLICKR_GROUP_ID
+
             # Find and replace doc - inserting if it doesn't exist
             # This should never happen but we want to make sure
             bulk.find({'_id': record['_id']}).upsert().replace_one(record)
-
             count += 1
 
     if count:
@@ -93,5 +167,5 @@ def import_flickr_images():
             print 'ERROR: No images to load'
 
 
-if __name__ == '__main__':
-    import_flickr_images()
+if __name__ == "__main__":
+    sys.exit(main())
